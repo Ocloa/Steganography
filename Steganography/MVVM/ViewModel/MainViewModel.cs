a5d109a0e80a19a0998b705e56f7e561691f917d
Ocloa <lexasen231@mail.ru>
2022-03-18 21:54:38 +0700

    Added About page

---
+++ Steganography/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Steganography.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steganography.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand EmbedViewCommand { get; set; }
+
+        public  HomeViewModel HomeVM { get; set; }
+        public EmbedTextViewModel EmbedVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            EmbedVM = new EmbedTextViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+            EmbedViewCommand = new RelayCommand(o =>
+           {
+               CurrentView = EmbedVM;
+           });
+        }
+    }
+}
