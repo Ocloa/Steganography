1760ba7c9d63453ed1e192648ef7be2f79d3b913
Ocloa <lexasen231@mail.ru>
2022-03-24 20:04:58 +0700

    Form optimization

--- Steganography/MainWindow.xaml.cs
+++ Steganography/MainWindow.xaml.cs
@@ -24,5 +24,18 @@ namespace Steganography
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        
     }
 }
